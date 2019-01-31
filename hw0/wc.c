f86c31f58380804642cfc303f72550b7e6a9e42c
LearnerPermitZKL <jobzkl@outlook.com>
2019-01-31 09:14:23 +0000

    first step

--- hw0/wc.c
+++ hw0/wc.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+
 int main(int argc, char *argv[]) {
+
+    FILE *fp;
+
+    if (argc == 0) {
+        fp = stdin;
+    }
+    else if (argc == 1) {
+        fp = argv[1]
+    }
+
     return 0;
 }
